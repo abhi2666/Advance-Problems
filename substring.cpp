c29b36371a25fc0c9bfd821223a29f0683faf1aa
abhi2666 <95623413+abhi2666@users.noreply.github.com>
2022-11-10 13:30:53 +0530

    longest substring without repeating character

---
+++ substring.cpp
@@ -0,0 +1,36 @@
+/*Program to find longest substring without repeating elements
+1. Brute Force --> find all the substring and then shorlist the one which is 
+longest and have unique elements
+*/
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int longestSubstring(string s)
+{
+    int left, right, n = s.size();
+    vector<int> mpp(256, -1);
+    left = right = 0;
+    int len = 0;
+    while(right < n)
+    {
+        //if element already exist..
+        if(mpp[s[right]]) left = max(mpp[s[right]] + 1, left);
+
+        
+        mpp[s[right]] = right; // if element is not present
+        
+        len = max(len, right - left);
+        right++;
+    }
+    return len;
+}
+int main()
+{
+
+    string s = "bbbbb";
+    cout<<longestSubstring(s)<<endl;
+    
+    return 0;
+}
